136f3e91982754cfb990b40ef002d11e1625a808
Omarb28 <omarb28@gmail.com>
2020-05-11 13:35:07 +0000

    Added plants model seed data and GET method

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from models import setup_db, Plant
-from flask_corss import CORS
+from flask_cors import CORS
 
 def create_app(test_config=None):
   app = Flask(__name__)
@@ -10,15 +10,27 @@ def create_app(test_config=None):
 
   # CORS Headers
   @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
-        return response
+  def after_request(response):
+      response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+      response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+      return response
 
-  @app.route('/')
-  def hello():
-    return jsonify({'message': 'HELLO WORLD'})
+  @app.route('/plants', methods=['GET', 'POST'])
+  def get_plants():
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * 10
+    #end = start + 10
+    #plants = Plant.query.all()
+    plants = Plant.query.offset(start).limit(10).all()
+    formatted_plants = [plant.format() for plant in plants]
+    total_plants = Plant.query.count()
 
+    return jsonify({
+      'success': True,
+      #'plants': formatted_plants[start:end],
+      'plants': formatted_plants,
+      'total_plants': total_plants
+    })
 
 
   return app
